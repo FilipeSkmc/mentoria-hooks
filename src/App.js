6b8e207b97b267e5bf9c555485794c8c10643459
FilipeSkmc <filipe-santana@outlook.com>
2023-03-24 10:23:23 -0300

    fix

--- src/App.js
+++ src/App.js
@@ -2,19 +2,25 @@ import { useState } from 'react';
 import './App.css';
 import FormBasic from './components/FormBasic';
 import FetchRick from './components/FetchRick';
+import EffectSimulator from './components/EffectSimulator';
 
 function App() {
-  /** parte 1 */
+  /** Extra */
   const [mode, setMode] = useState(false);
 
   return (
     <div className={ `App-${mode ? 'light' : 'dark'}` }>
       <h3>Mentoria Hooks</h3>
+      {/* extra */}
       <p>Controlando o state individual</p>
       <button onClick={ () => setMode(!mode) }>
         {mode ? 'Agora apague!' : 'Que haja Luz!'}
       </button>
+      {/* parte 1 */}
+      <EffectSimulator />
+      {/* parte 2 */}
       <FormBasic />
+      {/* parte 3 */}
       <FetchRick />
     </div>
   );
---
+++ src/components/EffectSimulator.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+
+function EffectSimulator() {
+  const [stateA, setStateA] = useState(0);
+  const [stateB, setStateB] = useState(0);
+
+  useEffect(() => {
+    console.log('Sou o efeito sem array de depencencias, olhem pra mim!');
+  });
+
+  useEffect(() => {
+    console.log(
+      'Sou o efeito com array de depencencias vazio, executo quando o componente',
+    );
+  }, []);
+
+  useEffect(() => {
+    console.log(
+      'Sou o efeito com array de depencencia com stateA, executo quando ele atualiza',
+    );
+  }, [stateA]);
+
+  useEffect(() => {
+    console.log(
+      'Sou o efeito com array de depencencia com stateB, executo quando ele atualiza',
+    );
+  }, [stateB]);
+
+  return (
+    <div>
+      <h3>Effect Simulator</h3>
+      <button
+        onClick={ () => setStateA(stateA + 1) }
+      >
+        { `State A: ${stateA}` }
+      </button>
+      <button
+        onClick={ () => setStateB(stateB + 1) }
+      >
+        { `State B: ${stateB}` }
+      </button>
+    </div>
+  );
+}
+
+export default EffectSimulator;
